f658b0370314b21ed25306ded373cdf0b72abb0d
Ömer ÜNEŞİ <info@omerunesi.com>
2021-05-15 13:07:05 +0300

    Kuzem video frame kontrol

---
+++ kuzem_videoDeneme/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace kuzem_videoDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_openFile = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_openFile
+            // 
+            this.btn_openFile.Location = new System.Drawing.Point(290, 320);
+            this.btn_openFile.Name = "btn_openFile";
+            this.btn_openFile.Size = new System.Drawing.Size(80, 33);
+            this.btn_openFile.TabIndex = 2;
+            this.btn_openFile.Text = "Video Seç";
+            this.btn_openFile.UseVisualStyleBackColor = true;
+            this.btn_openFile.Click += new System.EventHandler(this.btn_openFile_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 282);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 365);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_openFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_openFile;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ kuzem_videoDeneme/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Accord;
+using Accord.Video.FFMPEG;
+using Accord.Video;
+
+
+
+namespace kuzem_videoDeneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_openFile_Click(object sender, EventArgs e)
+        {
+            VideoFileReader reader = new VideoFileReader();
+            // open video file
+            //reader.Open(@"C:\Users\Ömer ÜNEŞİ\Source\Repos\kuzem_videoDeneme\kuzem_videoDeneme\Video.mp4");
+
+            using (var vFReader = new VideoFileReader())
+            {
+                int adet = 0;
+                int[] redBitmap = new int[2000];
+                int control_re = new Color().R;
+               
+                OpenFileDialog openFile = new OpenFileDialog();
+                openFile.ShowDialog();
+                vFReader.Open(openFile.FileName);
+                
+                
+                //Bitmap btm_ref = new Bitmap(@"C:\Users\Ömer ÜNEŞİ\Source\Repos\kuzem_videoDeneme\kuzem_videoDeneme\picture.bmp");
+                
+                for (int i = 0; i < vFReader.FrameCount; i += 24)
+                {
+                    Bitmap bmpBaseOriginal = vFReader.ReadVideoFrame();
+                    //if (bmpBaseOriginal == null)
+                    //    continue;
+                    for (int j = 0; j < vFReader.Width; j++)
+                    {
+                        for (int k = 0; k < vFReader.Height; k++)
+                        {
+                            control_re = bmpBaseOriginal.GetPixel(j, k).R;
+                            redBitmap[j] += control_re;
+                        }
+                    }
+                }
+
+                for (int i = 1; i < vFReader.FrameCount; i += 24)
+                {
+                    if (redBitmap[i - 1] == redBitmap[i])
+                    {
+                        adet++;
+                    }
+                }
+                vFReader.Close();
+                MessageBox.Show(adet.ToString());
+            }
+        }
+    }
+}
